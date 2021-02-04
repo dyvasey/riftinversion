2713a33abe20ffafb26aeb373b49a39f7653fbe5
Dylan Vasey <dyvasey@gmail.com>
2021-02-04 13:30:52 -0800

    Added .png to gitignore, added shell script for Stampede2 run

---
+++ run.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -J rirv1t10
+#SBATCH -o log_ri_rift_1cm_100km_base
+#SBATCH -e %j
+#SBATCH -p skx-normal
+#SBATCH -t 48:00:00
+#SBATCH -N 1
+#SBATCH -n 48
+#SBATCH -A TG-EAR080022N
+
+# load modules
+module load gcc/7.1.0 
+
+# Aspect executable
+ASP="/work/07937/davasey/stampede2/software/aspect/build/./aspect"
+
+# Run model.  Submit job with "sbatch run.sh"
+ibrun $ASP ri_rift_1cm_100km_base.prm
+
+
