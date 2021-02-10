5d486fd8ec885c61a6ea29df4648b5782ec2baba
Dylan Vasey <dyvasey@gmail.com>
2021-02-09 18:21:42 -0800

    Added evelocity function.

--- ripropagate.py
+++ ripropagate.py
@@ -10,7 +10,7 @@ import pandas as pd
 
 def thickness(thickness,directory=".",base=100):
     """
-    Convert .prm file to new lithospheric thickness
+    Convert .prm file to new lithospheric thickness.
     """
     csv = 'thermal_'+str(thickness)+'km.csv'
     thermal = pd.read_csv(csv,index_col=0).squeeze().to_dict()
@@ -37,3 +37,33 @@ def thickness(thickness,directory=".",base=100):
         newfile.writelines(copy)
         newfile.close()
     return(copy)
+
+def evelocity(vel,directory=".",base=1):
+    """
+    Convert base .prm file to new extension velocity.
+    """
+    # Convert total velocity in cm/yr to half velocity in m/yr
+    v = (vel/2)/100
+    v_base = (base/2)/100 
+    
+    for file in os.listdir(directory):
+        if file.endswith('base.prm'): # Find all the base .prm files
+            filename = os.path.join(directory,file) # Join root to filename
+
+            with open(filename) as f_prm: #open each file
+                contents = f_prm.read() #read file as lines
+                copy = contents
+                old = 'v='+str(v_base)
+                new = 'v='+str(v)
+                copy = copy.replace(old,new)
+            
+                oldname = str(base)+'cm'
+                newname = str(vel)+'cm'
+                # Make sure no decimals in the new filename
+                newname = newname.replace('.','-') 
+                newfilename = filename.replace(oldname,newname)
+                newfile = open(newfilename,"w")
+                newfile.writelines(copy)
+                newfile.close()
+    return(copy)
+    
