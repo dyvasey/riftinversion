34e7d0afc7a590541def2361710983e41c23e1bc
Dylan Vasey <dyvasey@gmail.com>
2021-02-15 14:04:21 -0800

    Removed old thickness function.

--- ripropagate.py
+++ ripropagate.py
@@ -33,61 +33,6 @@ def lthickness(text,lthick):
     
     return(text)
 
-def generate(file,output='.',lthick=100):
-    """
-    Generate .prm file from dummy base file. Thickness only.
-    """
-    lthick_str = str(lthick)+'km' # String version of thickness  
-    
-    path = os.path.join('.',file) # Join root to filename
-    newname = file.replace('base',lthick_str) # Change file name
-    
-    with open(path) as f_prm: # Open the file
-        contents = f_prm.read() # Read file into string
-        contents = lthickness(contents,lthick)
-        newpath = os.path.join(output,newname)
-        newfile = open(newpath,"w")
-        newfile.writelines(contents)
-        newfile.close()
-
-
-def thickness(thickness,directory,base=100):
-    """
-    Convert .prm file to new lithospheric thickness.
-    """
-    thick_str = str(thickness)+'km' # String version of thickness  
-    os.makedirs(directory,exist_ok=True) # Make new directory
-    
-    csv = 'thermal_'+thick_str+'.csv'
-    thermal = pd.read_csv(csv,index_col=0).squeeze().to_dict()
-    
-    base_thermal = pd.read_csv('thermal_'+str(base)+'km.csv',
-                               index_col=0).squeeze().to_dict()
-    
-    params = ['ts1','ts2','ts3','ts4','qs1','qs2','qs3']
-    
-    for file in os.listdir("."):
-        if file.endswith('100km_base.prm'): # Find the base .prm file
-            filename = os.path.join(".",file) # Join root to filename
-            # Get new file name for use later.
-            newfilename = file.replace(str(base),str(thickness))
-
-    with open(filename) as f_prm: # Open the file
-        contents = f_prm.read() # Read file
-        copy = contents
-        for param in params:
-            old = param+'='+str(round(base_thermal[param],5))
-            new = param+'='+str(round(thermal[param],5))
-            copy = copy.replace(old,new)
-                    
-        newpath = os.path.join(directory,newfilename) # Create new path
-        newfile = open(newpath,"w")
-        newfile.writelines(copy)
-        newfile.close()
-    return(copy)
-
-# Need to add changes to adiabatic conditions function as well - has 100km value.
-
 def evelocity(vel,directory,base=1):
     """
     Convert base .prm file to new extension velocity.
@@ -121,4 +66,23 @@ def evelocity(vel,directory,base=1):
         newfile.writelines(copy)
         newfile.close()
     return(copy)
+
+def generate(file,output='.',lthick=100):
+    """
+    Generate .prm file from dummy base file. Thickness only.
+    """
+    lthick_str = str(lthick)+'km' # String version of thickness  
+    
+    path = os.path.join('.',file) # Join root to filename
+    newname = file.replace('base',lthick_str) # Change file name
+    
+    with open(path) as f_prm: # Open the file
+        contents = f_prm.read() # Read file into string
+        contents = lthickness(contents,lthick)
+        newpath = os.path.join(output,newname)
+        newfile = open(newpath,"w")
+        newfile.writelines(contents)
+        newfile.close()
+
+
     
