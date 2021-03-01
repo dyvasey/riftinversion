304801254638d5d0291a252a9a185564e950decb
Dylan Vasey <dyvasey@gmail.com>
2021-03-01 09:03:37 -0800

    Added pycache folder to gitignore. Added 20 Myr reference model to createprm.

--- createprm.py
+++ createprm.py
@@ -1,3 +1,6 @@
 """
-Script for executing the current prm models to run on Stampede2
+Script for creating the current prm models to run on Stampede2
 """
+import ripropagate
+
+ripropagate.generate(etime=20) # Generate reference model for 20 Myr run
