6afe99eff33a8fd373a22e404b3ecd3826ab2c8c
Dylan Vasey <dyvasey@gmail.com>
2021-03-07 21:26:00 -0800

    Reverted creatprm back to reference modle only for testing.

--- createprm.py
+++ createprm.py
@@ -4,11 +4,3 @@ Script for creating the current prm models to run on Stampede2
 import ripropagate
 
 ripropagate.generate(etime=20) # Generate reference model for 20 Myr run
-
-# Generate 5 Myr increments
-times = [5,10,15,20]
-versions = ['a','b','c','d']
-folders = [str(x).replace('.','-')+'Myr' for x in times] 
-
-for x in range(len(times)):
-    ripropagate.generate(etime=times[x],ver=versions[x],output=folders[x])
