fe4f018a26b63c58f8b086e4be9917546c17ee79
Dylan Vasey <dyvasey@gmail.com>
2021-03-12 17:32:57 -0800

    Changed createprm to reflect models to run.

--- createprm.py
+++ createprm.py
@@ -3,4 +3,11 @@ Script for creating the current prm models to run on Stampede2
 """
 import ripropagate
 
-ripropagate.generate(etime=20,ver='b') # Generate reference model for 20 Myr run
+# Reference model to breakup
+ripropagate.generate(etime=12,output='./ref_breakup',ver='a') 
+
+# Reference model halfway to breakup
+ripropagate.generate(etime=6,output='./ref_half',ver='b') 
+
+# Asymmetric model test run
+ripropagate.generate(etime=20,lthick=80,evel=2,output='./asymm_rift',ver='c') 
