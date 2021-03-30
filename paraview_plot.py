60f5b2f7bac26c9596fa31840e78936cbfa30c2f
Dylan Vasey <dyvasey@gmail.com>
2021-03-30 08:21:22 -0700

    Updates to lab notebook.

--- paraview_plot.py
+++ paraview_plot.py
@@ -5,6 +5,7 @@ Functions for plotting data from Paraview
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import pyvista as pv
 
 def byerlee(density,depth):
     # density kg/m^3
@@ -111,6 +112,62 @@ def cmyk2rgb(cmyk):
     b = (1-cmyk[2]/100)*(1-cmyk[3]/100)
     rgb = (r,g,b)
     return(rgb)
+
+def plot(file,output,field='density',bounds=None,contours=True,
+         cfields=['crust_upper','crust_lower','mantle_lithosphere'],
+         null_field='asthenosphere',off_screen=True,
+         camera=None,**kwargs):
+    
+    mesh = pv.read(file)
+    mesh = mesh.clip_box(bounds=bounds,invert=False)
+    if field=='comp_field':
+        mesh = comp_field_vtk(mesh,fields=cfields,null_field=null_field)
+    
+    if contours==True:
+        cntrs = add_contours(mesh)
+    
+    pv.set_plot_theme("document")
+    plotter = pv.Plotter(off_screen=off_screen)
+    
+    plotter.add_mesh(mesh,scalars=field,**kwargs)
+    
+    if contours ==True:
+        plotter.add_mesh(cntrs,color='black',line_width=5)
+    
+    plotter.view_xy()
+    plotter.remove_scalar_bar()
+    #plotter.remove_bounding_box()
+    #plotter.reset_camera()
+    plotter.window_size = 1200,660
+    if camera is not None:
+        plotter.camera_position = camera
+        print('hey ya')
+        plotter.camera_set = True
+    if off_screen==False:
+        cpos = plotter.show(screenshot='output')
+        return(cpos)
+    else:
+        img = plotter.screenshot(output,transparent_background=True,
+                             return_img=True)
+        return(img)
+
+def add_contours(mesh,field='T',values=np.arange(500,1700,200)):
+    contour_mesh = mesh.copy()
+    cntrs = contour_mesh.contour(isosurfaces=values,scalars=field)
+    return(cntrs)
+    
+
+def comp_field_vtk(mesh,fields=['crust_upper','crust_lower','mantle_lithosphere'],
+               null_field='asthenosphere'):
+
+    # Create empty np array
+    output = np.zeros(shape=mesh.point_arrays[fields[0]].shape)
+    for x in range(len(fields)):
+        array = mesh.point_arrays[fields[x]]
+        output = np.where(array>0.5,x+1,output)
+    
+    mesh.point_arrays['comp_field'] = output
+    return(mesh)
     
     
 
