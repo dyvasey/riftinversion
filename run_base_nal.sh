1ab7569b08f1f3f25f3c437b89faf2184eed0c78
Dylan Vasey <dyvasey@gmail.com>
2021-09-06 08:39:02 -0700

    Added modified run base shell scripts and updated gitignore.

---
+++ run_base_nal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -J riftinv
+#SBATCH -o log_XXX
+#SBATCH -e %j
+#SBATCH -p skx-normal
+#SBATCH -t 48:00:00
+#SBATCH -N 1
+#SBATCH -n 48
+#SBATCH -A TG-EAR080022N
+
+# load modules
+module load gcc/7.1.0 
+
+# Aspect executable
+ASP="/work/07937/davasey/stampede2/software/gcc-7.1.0/aspect/naliboff-dev/aspect/build/./aspect"
+
+# Run model.  Submit job with "sbatch run.sh"
+ibrun $ASP XXX
+
+
