c9109a3bfed521a4abb431a4858e93baadb09e05
Dylan Vasey <dyvasey@gmail.com>
2021-12-01 14:55:43 -0800

    Added colorbar plotting to vtk test.

--- tchron/tests/vtk_test.py
+++ tchron/tests/vtk_test.py
@@ -4,6 +4,8 @@ Test for forward modeling of ASPECT output, using cookbook output
 
 #%% Import and setup files
 import matplotlib.pyplot as plt
+from matplotlib import cm,colors
+
 import numpy as np
 
 import pyvista as pv
@@ -23,15 +25,21 @@ He_mesh_interp = vp.He_age_vtk_parallel(meshes,'AHe',1e5,batch_size=100,
 
 fig,axs = plt.subplots(2,dpi=300)
 
-vp.plot2D('meshes_He/meshes_He_10.vtu','AHe',bounds=[0,200,90,100],cmap='magma_r',ax=axs[0],
+vp.plot2D('meshes_He/meshes_He_20.vtu','AHe',bounds=[0,200,90,100],cmap='plasma_r',ax=axs[0],
           clim=[0,2])
 
-vp.plot2D('meshes_He_interp/meshes_He_interp_10.vtu','AHe',bounds=[0,200,90,100],cmap='magma_r',ax=axs[1],
+vp.plot2D('meshes_He_interp/meshes_He_interp_20.vtu','AHe',bounds=[0,200,90,100],cmap='plasma_r',ax=axs[1],
           clim=[0,2])
 
 ages = He_mesh[-1].point_data['AHe']
 
-plt.tight_layout()
+cax = fig.add_axes([0.1,0.08,0.8,0.02])
+
+norm = colors.Normalize(vmin=0,vmax=2)
+mappable = cm.ScalarMappable(norm=norm,cmap='plasma_r')
+cbar = plt.colorbar(mappable,orientation='horizontal',cax=cax)
+cbar.set_label('AHe Age')
+cbar.set_ticks([0,1,2])
 
 fig.savefig('vtk_tchron_test.pdf')
 
