e3cb6d3677d1b78f2ace9b34cf823511ea57bb75
Dylan Vasey <dyvasey@gmail.com>
2022-05-20 14:26:28 -0700

    Set base run file to production ASPECT version and Cowgill allocation

--- run_base.sh
+++ run_base.sh
@@ -6,13 +6,13 @@
 #SBATCH -t 48:00:00
 #SBATCH -N 1
 #SBATCH -n 48
-#SBATCH -A TG-EAR080022N
+#SBATCH -A TG-EES210024
 
 # load modules
 module load gcc/7.1.0 
 
 # Aspect executable
-ASP="/work/07937/davasey/stampede2/software/gcc-7.1.0/aspect/aspect-2.4.0-pre-77a8b25c3/aspect/build/./aspect"
+ASP="/work/07937/davasey/stampede2/software/gcc-7.1.0/aspect/aspect-2.4.0-pre-70a2ec3/aspect/build/./aspect"
 
 # Run model.  Submit job with "sbatch run.sh"
 ibrun $ASP XXX
